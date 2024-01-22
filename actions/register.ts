74c8cb959b3a10e8864dd1ab580fc76ce93822ce
vinicius-dev <viniciusgabfarias123@gmail.com>
2024-01-22 10:07:22 -0300

    Create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
-import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt"
+
+import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     console.log(values);
@@ -14,5 +17,30 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         };
     }
 
-    return { success: "Email sent!"};
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email);
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    });
+
+    if (existingUser) {
+        return { error: "Email already in use" };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        },
+    });
+
+    //TODO: Send verification token email
+
+    return { success: "User created!"};
 };
